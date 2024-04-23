8267590e4f86cd1d1e351faada26a7dab1b8c12c
Sohan-2001 <112119230+Sohan-2001@users.noreply.github.com>
2024-04-24 03:21:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ st.title("@MASK Speed Monitor")
 
 def get_speed():
     stt = speedtest.Speedtest(secure=True)
+    stt.get_servers(servernames)
+    ping=sst.results.ping
     download_speed = stt.download() / (1024**2)  # Convert to Mbps
     upload_speed = stt.upload() / (1024**2)  # Convert to Mbps
-    return download_speed, upload_speed
+    return download_speed, upload_speed, ping
 
-download, upload = get_speed()
+download, upload, ping = get_speed()
 
 st.write(f"Download speed: **{download:.2f} Mbps**")
 st.write(f"Upload speed: **{upload:.2f} Mbps**")
+st.write(f"Ping: **{ping}")
