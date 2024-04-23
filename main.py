a28b863da1a66f4df65e7cfd11f7a8eb1d54a9b3
Sohan-2001 <112119230+Sohan-2001@users.noreply.github.com>
2024-04-24 03:13:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ st.title("@MASK Speed Monitor")
 
 @st.cache
 def get_speed():
-    stt = speedtest.Speedtest()
-    stt.get_best_server()
+    stt = speedtest.Speedtest(secure=True)
     download_speed = stt.download() / (1024**2)  # Convert to Mbps
     upload_speed = stt.upload() / (1024**2)  # Convert to Mbps
     return download_speed, upload_speed
